535a3164c750c851c69d2e9810894e09ddea1ad8
YoshyRias <jnsmarte@gmail.com>
2026-05-21 16:04:32 +0200

    Created Fake Services Created Test Project - doesnt work yet bc library needs to be a normal one

---
+++ Statifylib/Data/Services/TrackService/TrackServiceFake.cs
@@ -0,0 +1,28 @@
+﻿using Statifylib.Data.Models;
+
+namespace Statifylib.Data.Services.TrackService;
+
+public class TrackServiceFake: ITrackService
+{
+    private List<Track> Tracks = new List<Track>()
+    {
+        new Track() {Id = 1,  Name = "My First Track"},
+        new Track() {Id = 2,  Name = "My Second Track"},
+        new Track() {Id = 3,  Name = "My Third Track"},
+    };
+    
+    public Task<Track> GetTrack(int trackId)
+    {
+        return Task.FromResult(Tracks.FirstOrDefault(x => x.Id == trackId));
+    }
+
+    public Task<List<Track>> GetTracks()
+    {
+        return  Task.FromResult(Tracks.OrderBy(x => x.Id).ToList());
+    }
+
+    public void AddTrack(Track track)
+    {
+        Tracks.Add(track);
+    }
+}
